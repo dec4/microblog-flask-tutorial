92e4569377fc1649131ec4c86e71c931b800a574
dec4 <dcur2021@gmail.com>
2022-08-05 14:05:34 -0700

    feat: user registration page

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,9 @@ from flask_login import logout_user
 from werkzeug.urls import url_parse
 
 from app import app
+from app import db
 from app.forms import LoginForm
+from app.forms import RegistrationForm
 from app.models import User
 
 mock_posts = [
@@ -52,6 +54,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', register_form=form)
 
 @app.route('/random-login-required-page')
 @login_required
