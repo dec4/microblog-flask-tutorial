ee986ffbe69ab89a69d95a166dec5152002363a8
dec4 <dcur2021@gmail.com>
2022-08-04 22:36:54 -0700

    feat: basic index template with jinja features Displays jinja variable usage, conditionals, and for loops

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
+
 from app import app
+
+mock_user = {'username': 'dani'}
+mock_posts = [
+    {
+        'author': {'username': 'John'},
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home', user=mock_user, posts=mock_posts)
