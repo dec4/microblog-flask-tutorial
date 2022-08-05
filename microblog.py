b022f9d2edd784ac38ad8eff42b82a2523c6b022
dec4 <dcur2021@gmail.com>
2022-08-05 10:30:50 -0700

    feat: add flask shell context. See 'Playing with the Database' section to see exercises for familiarizing oneself with the database: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database

--- microblog.py
+++ microblog.py
@@ -1,1 +1,16 @@
 from app import app
+from app import db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    With this function, you can run `flask shell` in terminal and it will
+    start a Python interpreter in the context of the application (i.e. with
+    the following defined without having to import)
+    """
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post,
+    }
