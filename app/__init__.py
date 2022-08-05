e2a9aca7c4edbed2d594cb31de0c493db2f61c4e
dec4 <dcur2021@gmail.com>
2022-08-05 10:10:36 -0700

    resource: add flask-sqlalchemy and flask-migrate. create user table.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@
 Create instance of class Flask
 """
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # bottom import as a workaround to circular imports
+# bottom import as a workaround to circular imports:
+from app import routes, models
