87c1d95e3dd4b946dea4aa0ab8b0fa89174314b4
dec4 <dcur2021@gmail.com>
2022-08-05 09:03:05 -0700

    feat: login form using wtforms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
+from flask import flash
+from flask import redirect
 from flask import render_template
 
 from app import app
+from app.forms import LoginForm
 
 mock_user = {'username': 'dani'}
 mock_posts = [
@@ -18,3 +21,11 @@ mock_posts = [
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home', user=mock_user, posts=mock_posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # runs validation when browser sends POST on user submit
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', login_form=form)
