1c72327d5470be0c1871b2dac4cafea01c0ca2b1
dec4 <dcur2021@gmail.com>
2022-08-08 17:52:16 -0700

    feat: log to file (INFO+)

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 from config import Config
 
@@ -36,6 +38,16 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
 
 # bottom import as a workaround to circular imports:
 from app import routes, models, errors
